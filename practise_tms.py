5a87236ffd34a142d79b24eb7faa706b30865fdd
dhaval_patel <dhavalrpatel2511@gmail.com>
2019-04-18 20:52:37 +0200

    modified - 1.c

--- practise_tms.py
+++ practise_tms.py
@@ -1,7 +1,10 @@
-x0 = 5
-y0 = 2
-x = float(input('enter any value for x: '))
-y = float(input('enter any value for y: '))
+import numpy as np
+x0 = -2
+y0 = 1
+#x = float(input('enter any value for x: '))
+#y = float(input('enter any value for y: '))
+x = np.array([1,2,3])
+y = np.array([4,5,6])
 def fun(x,y,x0,yo):
     distance= ((x-x0)**2+(y-y0)**2)**0.5
     return distance
