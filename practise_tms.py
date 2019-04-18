76c7ec2d8b976326c6e932572b3d60e2797932b8
dhaval_patel <dhavalrpatel2511@gmail.com>
2019-04-18 23:31:09 +0200

    modified:practise_tms.py-2.b

--- practise_tms.py
+++ practise_tms.py
@@ -1,20 +1,30 @@
 import numpy as np
+import matplotlib.pyplot as plt
 x0 = 1
 y0 = 2
 #x = float(input('enter any value for x: '))
 #y = float(input('enter any value for y: '))
 #x = np.array([1,2,3])
 #y = np.array([4,5,6])
-x_start = -2
-x_end = 2
+x_start = -4
+x_end = 10
 y_start = 0
-y_end = 3
-n_points1 = 5
-n_points2 = 4
-x = np.linspace(x_start, x_end, n_points1)
-y = np.linspace(y_start, y_end, n_points2)
-xx, yy = np.meshgrid(x,y)
+y_end = 12
+n_points1 = 9
+n_points2 = 11
+dx = (x_end-x_start)/n_points1
+dy = (y_end-y_start)/n_points2
+xv = np.linspace(x_start, x_end, n_points1+1)
+yv = np.linspace(y_start, y_end, n_points2+1)
+xc = np.linspace(x_start-dx/2, x_end-dx/2, n_points1+1)
+yc = np.linspace(y_start-dy/2, y_end-dy/2, n_points2+1)
+xv_2d, yv_2d = np.meshgrid(xv,yv)
+xc_2d, yc_2d = np.meshgrid(xc,yc)
 def fun(x,y,x0,yo):
     distance= ((x-x0)**2+(y-y0)**2)**0.5
     return distance
-print(fun(xx,yy,x0,y0))
+values = fun(xc_2d,yc_2d,0,1)
+print(values)
+plt.pcolormesh(xv_2d,yv_2d,values)
+plt.colorbar()
+plt.show()
