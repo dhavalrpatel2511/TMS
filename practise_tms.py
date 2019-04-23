a2fac5a528f07e0960b6a1276d2580dfc066fbd6
dhaval_patel <dhavalrpatel2511@gmail.com>
2019-04-23 16:47:16 +0200

    practise_tms.py - 2.c-new version

--- practise_tms.py
+++ practise_tms.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+def fun(x,y,x0,yo):
+    distance= ((x-x0)**2+(y-y0)**2)**0.5
+    return distance
 x0 = 1
 y0 = 2
 #x = float(input('enter any value for x: '))
@@ -10,24 +13,23 @@ x_start = -4
 x_end = 10
 y_start = 0
 y_end = 12
-n_points1 = 9
-n_points2 = 11
-dx = (x_end-x_start)/n_points1
-dy = (y_end-y_start)/n_points2
-xv = np.linspace(x_start, x_end, n_points1+1)
-yv = np.linspace(y_start, y_end, n_points2+1)
-xc = np.linspace(x_start-dx/2, x_end-dx/2, n_points1+1)
-yc = np.linspace(y_start-dy/2, y_end-dy/2, n_points2+1)
+n_pixelx = 14
+n_pixely = 12
+
+dx = (x_end-x_start)/n_pixelx
+dy = (y_end-y_start)/n_pixely
+xv = np.linspace(x_start, x_end, n_pixelx+1)
+yv = np.linspace(y_start, y_end, n_pixely+1)
+xc = np.linspace(x_start+dx/2, x_end-dx/2, n_pixelx)
+yc = np.linspace(y_start+dy/2, y_end-dy/2, n_pixely)
 xv_2d, yv_2d = np.meshgrid(xv,yv)
 xc_2d, yc_2d = np.meshgrid(xc,yc)
-def fun(x,y,x0,yo):
-    distance= ((x-x0)**2+(y-y0)**2)**0.5
-    return distance
+
 values = fun(xc_2d,yc_2d,0,1)
 print(values)
-#plt.pcolormesh(xv_2d,yv_2d,values)
-#plt.colorbar()
-#plt.show()
-plt.imshow(values, origin='lower', extent=(-4,10,0,12))
+plt.pcolormesh(xv_2d,yv_2d,values)
 plt.colorbar()
 plt.show()
+#plt.imshow(values, origin='lower', extent=(-4,10,0,12))
+#plt.colorbar()
+#plt.show()
